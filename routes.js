03e90fa06e5357f53183f02739e98d47f17b4616
soumyasics <soumya.sics@gmail.com>
2024-02-23 11:53:45 +0530

    skr1

--- routes.js
+++ routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require("./Parent/parentController");
+const parentController = require("./Parent/parentController");
 
-
+//Parent routes
+router.post('/registerParent',parentController.registerParent)
+router.post('/loginParent',parentController.loginParent)
+router.post('/viewParentById/:id',parentController.viewParentById)
+router.post('/viewParents',parentController.viewParents)
+router.post('/editParentById/:id',parentController.editParentById)
+router.post('/deleteParentById/:id',parentController.deleteParentById)
+router.post('/forgotPwdParent',parentController.forgotPwd)
 
 module.exports = router;
