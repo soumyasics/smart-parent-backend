60f05608ab4d559c48f9de7f81911fc42cd5234e
Anand R P <anandrp.sics@gmail.com>
2024-03-07 09:11:26 +0530

    rp task schema modified

--- rp/Tasks/taskController.js
+++ rp/Tasks/taskController.js
@@ -8,38 +8,56 @@ const addQuestions = (req, res) => {
     description,
     target,
     duration,
-    points,
     qn1,
     op1_1,
+    score1_1,
     op1_2,
+    score1_2,
     op1_3,
+    score1_3,
     op1_4,
-    ans1,
+    score1_4,
     qn2,
     op2_1,
+    score2_1,
     op2_2,
+    score2_2,
     op2_3,
+    score2_3,
     op2_4,
-    ans2,
+    score2_4,
     qn3,
     op3_1,
+    score3_1,
     op3_2,
+    score3_2,
     op3_3,
+    score3_3,
     op3_4,
-    ans3,
+    score3_4,
     qn4,
     op4_1,
+    score4_1,
     op4_2,
+    score4_2,
     op4_3,
+    score4_3,
     op4_4,
-    ans4,
+    score4_4,
     qn5,
     op5_1,
+    score5_1,
     op5_2,
+    score5_2,
     op5_3,
+    score5_3,
     op5_4,
-    ans5,
+    score5_4,
   } = req.body;
+
+  if (!rpid) {
+    return res.status(422).json({ message: "Resource Person Id is required." });
+  }
 
   const newTask = new TaskModel({
     rpid,
@@ -47,37 +65,51 @@ const addQuestions = (req, res) => {
     description,
     target,
     duration,
-    points,
     qn1,
     op1_1,
+    score1_1,
     op1_2,
+    score1_2,
     op1_3,
+    score1_3,
     op1_4,
-    ans1,
+    score1_4,
     qn2,
     op2_1,
+    score2_1,
     op2_2,
+    score2_2,
     op2_3,
+    score2_3,
     op2_4,
-    ans2,
+    score2_4,
     qn3,
     op3_1,
+    score3_1,
     op3_2,
+    score3_2,
     op3_3,
+    score3_3,
     op3_4,
-    ans3,
+    score3_4,
     qn4,
     op4_1,
+    score4_1,
     op4_2,
+    score4_2,
     op4_3,
+    score4_3,
     op4_4,
-    ans4,
+    score4_4,
     qn5,
     op5_1,
+    score5_1,
     op5_2,
+    score5_2,
     op5_3,
+    score5_3,
     op5_4,
-    ans5,
+    score5_4,
   });
 
   newTask
@@ -94,7 +126,6 @@ const addQuestions = (req, res) => {
 const viewAllTasks = async (req, res) => {
   try {
     const tasks = await TaskModel.find();
-;
     return res.status(200).json({ message: "tasks", data: tasks });
   } catch (error) {
     return res
@@ -162,105 +193,129 @@ const deleteTaskById = (req, res) => {
 const checkanswer1 = async (ans, answers) => {
   console.log(ans, answers);
   switch (ans) {
-    case answers.op1_1.op: return score = answers.op1_1.score
-    case answers.op1_2.op: return score = answers.op1_2.score
-    case answers.op1_3.op: return score = answers.op1_3.score
-    case answers.op1_4.op: return score = answers.op1_4.score
+    case answers.op1_1.op:
+      return (score = answers.op1_1.score);
+    case answers.op1_2.op:
+      return (score = answers.op1_2.score);
+    case answers.op1_3.op:
+      return (score = answers.op1_3.score);
+    case answers.op1_4.op:
+      return (score = answers.op1_4.score);
   }
-  return 0
-}
+  return 0;
+};
 const checkanswer2 = async (ans, answers) => {
   console.log(ans, answers);
   switch (ans) {
-    case answers.op2_1.op: return score = answers.op2_1.score
-    case answers.op2_2.op: return score = answers.op2_2.score
-    case answers.op2_3.op: return score = answers.op2_3.score
-    case answers.op2_4.op: return score = answers.op2_4.score
+    case answers.op2_1.op:
+      return (score = answers.op2_1.score);
+    case answers.op2_2.op:
+      return (score = answers.op2_2.score);
+    case answers.op2_3.op:
+      return (score = answers.op2_3.score);
+    case answers.op2_4.op:
+      return (score = answers.op2_4.score);
   }
-  return 0
-
-}
+  return 0;
+};
 const checkanswer3 = async (ans, answers) => {
   console.log(ans, answers);
   switch (ans) {
-    case answers.op3_1.op: return score = answers.op3_1.score
-    case answers.op3_2.op: return score = answers.op3_2.score
-    case answers.op3_3.op: return score = answers.op3_3.score
-    case answers.op3_4.op: return score = answers.op3_4.score
+    case answers.op3_1.op:
+      return (score = answers.op3_1.score);
+    case answers.op3_2.op:
+      return (score = answers.op3_2.score);
+    case answers.op3_3.op:
+      return (score = answers.op3_3.score);
+    case answers.op3_4.op:
+      return (score = answers.op3_4.score);
   }
-  return 0
-}
+  return 0;
+};
 const checkanswer4 = async (ans, answers) => {
   console.log(ans, answers);
   switch (ans) {
-    case answers.op4_1.op: return score = answers.op4_1.score
-    case answers.op4_2.op: return score = answers.op4_2.score
-    case answers.op4_3.op: return score = answers.op4_3.score
-    case answers.op4_4.op: return score = answers.op4_4.score
+    case answers.op4_1.op:
+      return (score = answers.op4_1.score);
+    case answers.op4_2.op:
+      return (score = answers.op4_2.score);
+    case answers.op4_3.op:
+      return (score = answers.op4_3.score);
+    case answers.op4_4.op:
+      return (score = answers.op4_4.score);
   }
-  return 0
-}
+  return 0;
+};
 
 const checkanswer5 = async (ans, answers) => {
   console.log(ans, answers);
   switch (ans) {
-    case answers.op5_1.op: return score = answers.op5_1.score
-    case answers.op5_2.op: return score = answers.op5_2.score
-    case answers.op5_3.op: return score = answers.op5_3.score
-    case answers.op5_4.op: return score = answers.op5_4.score
-  }  return 0
-
-}
+    case answers.op5_1.op:
+      return (score = answers.op5_1.score);
+    case answers.op5_2.op:
+      return (score = answers.op5_2.score);
+    case answers.op5_3.op:
+      return (score = answers.op5_3.score);
+    case answers.op5_4.op:
+      return (score = answers.op5_4.score);
+  }
+  return 0;
+};
 
 const getIds = async (taskid) => {
-  let rpid = null
-
-
-}
+  let rpid = null;
+};
 
 const addAnswers = async (req, res) => {
-  let rpid = null, comments = ""
-  let score1 = 0, score2 = 0, score3 = 0, score4 = 0, score5 = 0, ans = null, total = 0
-  await TaskModel.findById({ _id: req.params.id }).exec().then(data => {
-    ans = data
-  })
+  let rpid = null,
+    comments = "";
+  let score1 = 0,
+    score2 = 0,
+    score3 = 0,
+    score4 = 0,
+    score5 = 0,
+    ans = null,
+    total = 0;
+  await TaskModel.findById({ _id: req.params.id })
+    .exec()
+    .then((data) => {
+      ans = data;
+    })
     .catch((error) => {
       console.error(error);
-    })
+    });
 
-  let ans1 = req.body.ans1
-  let ans2 = req.body.ans2
-  let ans3 = req.body.ans3
-  let ans4 = req.body.ans4
-  let ans5 = req.body.ans5
-  score1 = await checkanswer1(ans1, ans)
-  score2 = await checkanswer2(ans2, ans)
-  score3 = await checkanswer3(ans3, ans)
+  let ans1 = req.body.ans1;
+  let ans2 = req.body.ans2;
+  let ans3 = req.body.ans3;
+  let ans4 = req.body.ans4;
+  let ans5 = req.body.ans5;
+  score1 = await checkanswer1(ans1, ans);
+  score2 = await checkanswer2(ans2, ans);
+  score3 = await checkanswer3(ans3, ans);
 
-  score4 = await checkanswer4(ans4, ans)
-  score5 = await checkanswer5(ans5, ans)
-  total = score1 + score2 + score3 + score4 + score5
+  score4 = await checkanswer4(ans4, ans);
+  score5 = await checkanswer5(ans5, ans);
+  total = score1 + score2 + score3 + score4 + score5;
   console.log("score", total);
-  await TaskModel.findById({ _id: req.params.id }).exec().then(data => {
-
-    rpid = data.rpid
-
-  }).catch((error) => {
-    console.error(error);
-
-  })
+  await TaskModel.findById({ _id: req.params.id })
+    .exec()
+    .then((data) => {
+      rpid = data.rpid;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
   if (total < 8) {
-    comments = "you better get a councilor advice"
-    suggestion = 0
-  }
-  else if (total > 8 && total < 15) {
-    comments = "Your kid is Okay !!! But need to get more active"
-    suggestion = 1
-  }
-  else {
-    comments = "Your kid is Perfect !!!"
-    suggestion = 2
+    comments = "you better get a councilor advice";
+    suggestion = 0;
+  } else if (total > 8 && total < 15) {
+    comments = "Your kid is Okay !!! But need to get more active";
+    suggestion = 1;
+  } else {
+    comments = "Your kid is Perfect !!!";
+    suggestion = 2;
   }
   const answerss = new ansewrmodel({
     parentid: req.body.parentid,
@@ -278,29 +333,27 @@ const addAnswers = async (req, res) => {
     score5: score5,
     total: total,
     comments: comments,
-    suggestion: suggestion
-  })
-  await answerss.save().then(data => {
-
-    return res.status(200).json({
-      message: "Answers added successfully",
-      data
-    });
-  })
-    .catch(error => {
-      console.log(error);
-      return res
-        .status(500)
-        .json({ message: "Server Error.", error });
-
-
+    suggestion: suggestion,
+  });
+  await answerss
+    .save()
+    .then((data) => {
+      return res.status(200).json({
+        message: "Answers added successfully",
+        data,
+      });
     })
-}
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ message: "Server Error.", error });
+    });
+};
 
 module.exports = {
   addQuestions,
   deleteTaskById,
   viewTaskQnById,
-  viewTaskQnByRPId, viewAllTasks,
-  addAnswers
+  viewTaskQnByRPId,
+  viewAllTasks,
+  addAnswers,
 };
--- rp/Tasks/taskSchema.js
+++ rp/Tasks/taskSchema.js
@@ -2,265 +2,228 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    rpid: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "rp",
-        required: true,
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    target: {
-        type: String,
-        required: true,
-    },
-    duration: {
-        type: String,
-        required: true,
-    },
-    qn1: {
-        type: String,
-        required: true,
-    },
-    op1_1: {
-        op: {
-            type: String,
-            required: true,
-        },
+  rpid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "rp",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  qn1: {
+    type: String,
+    required: true,
+  },
+  op1_1: {
+    type: String,
+    required: true,
+  },
 
-        score: {
-            type: Number,
-            required: true,
-        }
-    }
-    ,
-    op1_2: {
-        op: {
-            type: String,
-            required: true,
-        },
+  score1_1: {
+    type: Number,
+    required: true,
+  },
 
-        score: {
-            type: Number,
-            required: true,
-        }
+  op1_2: {
+    type: String,
+    required: true,
+  },
+  score1_2: {
+    type: Number,
+    required: true,
+  },
 
-    },
-    op1_3: {
-        op: {
-            type: String,
-            required: true,
-        },
+  op1_3: {
+    type: String,
+    required: true,
+  },
+  score1_3: {
+    type: Number,
+    required: true,
+  },
+  op1_4: {
+    type: String,
+    required: true,
+  },
+  score1_4: {
+    type: Number,
+    required: true,
+  },
 
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op1_4: {
-        op: {
-            type: String,
-            required: true,
-        },
+  qn2: {
+    type: String,
+    required: true,
+  },
 
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
+  op2_1: {
+    type: String,
+    required: true,
+  },
 
-    qn2: {
-        type: String,
-        required: true,
-    },
-    op2_1: {
-        op: {
-            type: String,
-            required: true,
-        },
+  score2_1: {
+    type: Number,
+    required: true,
+  },
 
-        score: {
-            type: Number,
-            required: true,
-        }
+  op2_2: {
+    type: String,
+    required: true,
+  },
+  score2_2: {
+    type: Number,
+    required: true,
+  },
 
-    },
-    op2_2: {
-        op: {
-            type: String,
-            required: true,
-        },
+  op2_3: {
+    type: String,
+    required: true,
+  },
+  score2_3: {
+    type: Number,
+    required: true,
+  },
+  op2_4: {
+    type: String,
+    required: true,
+  },
+  score2_4: {
+    type: Number,
+    required: true,
+  },
 
-        score: {
-            type: Number,
-            required: true,
-        }
+  qn3: {
+    type: String,
+    required: true,
+  },
+  op3_1: {
+    type: String,
+    required: true,
+  },
 
+  score3_1: {
+    type: Number,
+    required: true,
+  },
 
-    },
-    op2_3: {
-        op: {
-            type: String,
-            required: true,
-        },
+  op3_2: {
+    type: String,
+    required: true,
+  },
+  score3_2: {
+    type: Number,
+    required: true,
+  },
 
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op2_4: {
-        op: {
-            type: String,
-            required: true,
-        },
+  op3_3: {
+    type: String,
+    required: true,
+  },
+  score3_3: {
+    type: Number,
+    required: true,
+  },
+  op3_4: {
+    type: String,
+    required: true,
+  },
+  score3_4: {
+    type: Number,
+    required: true,
+  },
 
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
+  qn4: {
+    type: String,
+    required: true,
+  },
 
-    qn3: {
-        type: String,
-        required: true,
-    },
-    op3_1: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op3_2: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op3_3: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op3_4: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    
-    qn4: {
-        type: String,
-        required: true,
-    },
-    op4_1: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op4_2: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op4_3: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op4_4: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    
-    qn5: {
-        type: String,
-        required: true,
-    },
-    op5_1: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op5_2: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op5_3: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    },
-    op5_4: {
-        op: {
-            type: String,
-            required: true,
-        },
-        score: {
-            type: Number,
-            required: true,
-        }
-    }
-    
+  op4_1: {
+    type: String,
+    required: true,
+  },
+
+  score4_1: {
+    type: Number,
+    required: true,
+  },
+
+  op4_2: {
+    type: String,
+    required: true,
+  },
+  score4_2: {
+    type: Number,
+    required: true,
+  },
+
+  op4_3: {
+    type: String,
+    required: true,
+  },
+  score4_3: {
+    type: Number,
+    required: true,
+  },
+  op4_4: {
+    type: String,
+    required: true,
+  },
+  score4_4: {
+    type: Number,
+    required: true,
+  },
+
+  qn5: {
+    type: String,
+    required: true,
+  },
+  op5_1: {
+    type: String,
+    required: true,
+  },
+
+  score5_1: {
+    type: Number,
+    required: true,
+  },
+
+  op5_2: {
+    type: String,
+    required: true,
+  },
+  score5_2: {
+    type: Number,
+    required: true,
+  },
+
+  op5_3: {
+    type: String,
+    required: true,
+  },
+  score5_3: {
+    type: Number,
+    required: true,
+  },
+  op5_4: {
+    type: String,
+    required: true,
+  },
+  score5_4: {
+    type: Number,
+    required: true,
+  },
 });
 const TaskModel = mongoose.model("taskqns", TaskSchema);
 
