014be45412337eac1d139c8a7ed167ddeacdc556
soumyasics <soumya.sics@gmail.com>
2024-03-09 13:19:31 +0530

    chat1

---
+++ Chats/parentrpchat.js
@@ -0,0 +1,27 @@
+
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    rpid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'rp',
+        required: true
+    },
+    parentid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'parents',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Message = mongoose.model('parentrpchats', messageSchema);
+
+module.exports = Message;
